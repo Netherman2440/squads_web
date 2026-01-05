33fb8a4cb3def87267b207fb8edd744ae3e63f4d
Netherman2440 <zajacignacy@gmail.com>
2026-01-05 11:27:24 +0100

    fix auth page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
       routerConfig: appRouter,
     );
   }
